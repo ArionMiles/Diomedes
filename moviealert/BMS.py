80f2f9005209a0593e6863dde2ac18cb05557108
Kanishk Singh <singh.kanishk07@gmail.com>
2019-01-06 22:18:06 +0530

    Major overhaul. Put BMS functions in their own file
    Finalized on get_showtimes() method.
    Incorporated get_region_list() into BMS class
    Added BMSError

---
+++ moviealert/BMS.py
@@ -0,0 +1,134 @@
+import json
+import datetime
+from urllib.parse import quote
+import requests
+
+class BMS():
+    """
+    BMS Class with all basic functions
+    """
+    def __init__(self, region_code, region_name):
+        self.region_code = region_code.upper()
+        self.region_name = region_name
+        self.BASE_URL = "https://in.bookmyshow.com/serv/getData?cmd="
+        self.ROOT_URL = "https://in.bookmyshow.com/"
+        self.region = quote(f"|Code={region_code}|text={region_name}|")
+        self.cookies = {
+            'Rgn': self.region,
+        }
+        self.session = requests.Session()
+        self.session.get(self.ROOT_URL, cookies=self.cookies)
+    
+    def quickbook(self, event_type):
+        """Returns quickbook with all current events.
+        
+        :param str event_type: Event types( MT(Movies), CT(Events), PL(Plays), SP(Sports))
+        :return: JSON dict of all events
+        """
+        response = self.session.get(f'{self.BASE_URL}QUICKBOOK&type={event_type}')
+        return response.json()
+
+    def get_movie_url(self, key, language, dimension="2D", event_type="MT"):
+        """Returns movie url
+
+        :param str key: Movie name
+        :param str language: Movie language
+        :param dimension: Movie dimension, can be 2D, 2D 4DX, 3D, 3D 4DX, or IMAX 3D
+        :type dimension: str
+        :param event_type: Event types( MT(Movies), CT(Events), PL(Plays), SP(Sports))
+        :type event_type: str
+        :return: Movie URL
+        :rtype: str
+        :raises BMSError: If movie is not found
+        
+        city: region_name (with spaces replaced with hypen)
+        TODO: Test edge cases
+        """
+        quickbook = self.quickbook(event_type)
+        city = self.region_name.replace(" ", "-")
+        movies = quickbook['moviesData']['BookMyShow']['arrEvents']
+        for movie in movies:
+            if key == movie['EventTitle']:
+                for child in movie['ChildEvents']:
+                    if language == child['EventLanguage'] and dimension == child['EventDimension']:
+                        event_url = child['EventURL']
+                        event_code = child['EventCode']
+                        today = datetime.date.today()
+                        date = today.strftime("%Y%m%d")
+                        return f"https://in.bookmyshow.com/buytickets/{event_url}-{city}/movie-{city}-{event_code}/{date}"
+        else:
+            raise BMSError("Movie not found! Please check the Movie name and other options")
+
+    def get_event_code(self, key, language, dimension="2D", event_type="MT"):
+        """Returns EventCode
+
+        :param str key: Movie name
+        :param str language: Movie language
+        :param dimension: Movie dimension, can be 2D, 2D 4DX, 3D, 3D 4DX, or IMAX 3D
+        :type dimension: str
+        :param event_type: Event types( MT(Movies), CT(Events), PL(Plays), SP(Sports))
+        :type event_type: str
+        :return: Event Code
+        :rtype: str
+        :raises BMSError: If the event code is not found
+        """
+        quickbook = self.quickbook(event_type)
+        movies = quickbook['moviesData']['BookMyShow']['arrEvents']
+        for movie in movies:
+            if key == movie['EventTitle']:
+                for child in movie['ChildEvents']:
+                    if language == child['EventLanguage'] and dimension == child['EventDimension']:
+                        return child['EventCode']
+        else:
+            raise BMSError("Event code not found! Please check the Movie name and other options")
+
+    def get_preferred_cinemas(self):
+        """Get a list of Venue Codes for popular cinemas in a region.
+
+        :return: List of popular cinemas
+        :rtype: list
+        """
+        preferred_cinemas = self.session.get(f'{self.BASE_URL}GETPREFERREDCINEMAS')
+        popular = preferred_cinemas.json()['popular']
+        popular_cinemas_list = []
+        for cinema in dict(popular).keys():
+            popular_cinemas_list.append(cinema)
+        return popular_cinemas_list
+    
+    def get_showtimes(self, key, language, date, dimension="2D", event_type="MT"):
+        """Get all showtime info for a movie (day, time, ticket types, prices, etc.)
+
+        :param str key: Movie name
+        :param str language: Movie language
+        :param dimension: Movie dimension, can be 2D, 2D 4DX, 3D, 3D 4DX, or IMAX 3D
+        :type dimension: str
+        :param event_type: Event types( MT(Movies), CT(Events), PL(Plays), SP(Sports))
+        :type event_type: str
+        :return: List of shows
+        :rtype: list
+        """
+        event_code = self.get_event_code(key, language, dimension, event_type)
+        venue_codes = self.get_preferred_cinemas()
+        date = date.strftime("%Y%m%d")
+        list_of_shows = []
+        for venue_code in venue_codes:
+            showtimes = self.session.get(f'{self.BASE_URL}GETSHOWTIMESBYEVENTANDVENUE&f=json&dc={date}&vc={venue_code}&ec={event_code}')
+            shows = showtimes.json()['BookMyShow']['arrShows']
+            # Do some formatting maybe?
+            list_of_shows.append(shows)
+        return list_of_shows
+
+    @staticmethod
+    def get_region_list():
+        """        
+        :return: List of regions
+        :rtype: dict
+        """
+        url = 'https://in.bookmyshow.com/serv/getData?cmd=GETREGIONS'
+        response = requests.get(url)
+        region_list = response.text.split("=", 1)[1]
+        region_list = region_list.split(";", 1)[0]
+        return json.loads(region_list)
+
+class BMSError(Exception):
+    pass
--- moviealert/utils.py
+++ moviealert/utils.py
@@ -1,70 +1,24 @@
-# TODO: get_regionCode_and_regionName()
-# use GETREGIONS endpoint for this.
-import json
+# TODO: FIND A WAY TO TAKE EVENT URLS, ALONG WITH LANGUAGE/DIMENSION AND CREATE TASKS
 import datetime
-from urllib.parse import quote
-import requests
+from .BMS import BMS
 from .models import Region, SubRegion, Cinemas
 
-BASE_URL = "https://in.bookmyshow.com/serv/getData?cmd="
-
-
-def get_region_list():
-    url = f'{BASE_URL}GETREGIONS'
-    response = requests.get(url)
-    region_list = response.text.split("=", 1)[1]
-    region_list = region_list.split(";", 1)[0]
-    return json.loads(region_list)
-
-def get_region_alias():
-    """DOESN'T WORK"""
-    url = f'{BASE_URL}GETREGIONS'
-    response = requests.get(url)
-    region_alias = response.text.split("=", 2)[1]
-    region_alias = region_alias.split(";", 1)[0]
-    return json.loads(region_alias)
-
-def get_subregion_list():
-    """DOESN'T WORK"""
-    url = f'{BASE_URL}GETREGIONS'
-    response = requests.get(url)
-    sub_regions = response.text.split("=", 4)[1]
-    sub_regions = sub_regions.split(";", 1)[0]
-    return json.loads(sub_regions)
-
 def save_region_data():
-    region_list = get_region_list()
+    region_list = BMS.get_region_list()
     for region in dict(region_list).keys():
         code = region_list[region][0]['code']
         name = region_list[region][0]['name']
         alias = region_list[region][0]['alias']
-        # Figure out how to store region aliases
-        # and subregions
 
         new_region = Region(code=code, name=name, alias=alias)
         new_region.save()
 
-def save_subregion_data():
+
+def save_venue_code():
     region_codes = Region.objects.all()
     for region in region_codes:
-        quickbook_resp = quickbook(region.code, region.name, "MT")
-        print(region.code, " | ", region.name)
-        cinemas = quickbook_resp['cinemas']['BookMyShow']['aiVN']
-        for cinema in cinemas:
-                sub_region_code = cinema['VenueSubRegionCode']
-                sub_region_name = cinema['VenueSubRegionName']
-
-                new_subregion, created = SubRegion.objects.get_or_create(region_code=region, sub_region_code=sub_region_code,
-                                        sub_region_name=sub_region_name)
-                new_subregion.save()
-
-# TODO: FIND A WAY TO TAKE EVENT URLS, ALONG WITH LANGUAGE/DIMENSION AND CREATE TASKS
-# We can find movies and the url right now but not showtimes
-
-def get_venue_code():
-    region_codes = Region.objects.all()
-    for region in region_codes:
-        quickbook_resp = quickbook(region.code, region.name, "MT")
+        bms = BMS(region.code, region.name)
+        quickbook_resp = bms.quickbook("MT")
         print(region.code, " | ", region.name)
         cinemas = quickbook_resp['cinemas']['BookMyShow']['aiVN']
         for cinema in cinemas:
@@ -77,69 +31,13 @@ def get_venue_code():
                                         venue_sub_region_code=venue_sub_region_code, venue_sub_region_name=venue_sub_region_name)
             new_cinema.save()
 
-class BMS():
-    def __init__(self, region_code, region_name):
-        self.region_code = region_code.upper()
-        self.region_name = region_name
-        self.BASE_URL = "https://in.bookmyshow.com/serv/getData?cmd="
-        self.ROOT_URL = "https://in.bookmyshow.com/"
-        self.region = quote(f"|Code={region_code}|text={region_name}|")
-        self.cookies = {
-            'Rgn': self.region,
-        }
-        self.session = requests.Session()
-        self.session.get(self.ROOT_URL, cookies=self.cookies)
-    
-    def quickbook(self, event_type):
-        response = self.session.get(f'{self.BASE_URL}QUICKBOOK&type={event_type}')
-        return response.json()
 
-    def search_quickbook(self, key, language, dimension, event_type="MT"):
-        """
-        Returns EventCode
-        Ideally should return movie url
-        city - region_name (with spaces replaced with hypen)
-        TODO: Test edge cases
-        """
-        quickbook = self.quickbook(event_type)
-        city = self.region_name.replace(" ", "-")
-        movies = quickbook['moviesData']['BookMyShow']['arrEvents']
-        for movie in movies:
-            if key == movie['EventTitle']:
-                for child in movie['ChildEvents']:
-                    if language == child['EventLanguage'] and dimension == child['EventDimension']:
-                        event_url = child['EventURL']
-                        event_code = child['EventCode']
-                        today = datetime.date.today()
-                        date = today.strftime("%Y%m%d")
-                        return f"https://in.bookmyshow.com/buytickets/{event_url}-{city}/movie-{city}-{event_code}/{date}"
-    
-    def get_preferred_cinemas(self):
-        """
-        Returns list of dict of popular cinemas.
-        Return just venue_code list?
-        """
-        preferred_cinemas = self.session.get(f'{self.BASE_URL}GETPREFERREDCINEMAS')
-        popular = preferred_cinemas.json()
-        popular_cinemas_list = []
-        for cinema in dict(popular).keys():
-            popular_cinemas_list.append(cinema)
-        return popular_cinemas_list
-    
-    def get_showtimes(self, event_code, date):
-        """
-        Think over what values need to be returned
-        """
-        venue_codes = self.get_preferred_cinemas()
-        date = datetime.datetime.today().strftime("%Y%m%d") # This is temporary, take date object
-        list_of_shows = []
-        for venue_code in venue_codes:
-            showtimes = self.session.get(f'{self.BASE_URL}GETSHOWTIMESBYEVENTANDVENUE&f=json&dc={date}&vc={venue_code}&ec={event_code}')
-            shows = showtimes.json()['BookMyShow']['arrShows']
-            # Do some formatting
-            # list_of_show.append(shows??)
-        return list_of_shows
-
-def test():
+def test_get_url():
     bms = BMS("MUMBAI", "Mumbai")
-    return bms.search_quickbook("Simmba", "Hindi", "2D")
+    return bms.get_movie_url("Simmba", "Hindi")
+
+def test_get_showtimes():
+    bms = BMS("MUMBAI", "Mumbai")
+    date = datetime.datetime.today()
+    shows = bms.get_showtimes("Simmba", "Hindi", date)
+    return shows
