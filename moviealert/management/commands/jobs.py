c7201db27f7949a861b6d489916eeb5e0b0d5a6d
Kanishk Singh <singh.kanishk07@gmail.com>
2019-01-21 00:11:50 +0530

    Add RQScheduler files

---
+++ moviealert/management/commands/jobs.py
@@ -0,0 +1,35 @@
+import logging
+from datetime import datetime
+import django_rq
+from django_rq.management.commands import rqscheduler
+
+from moviealert.utils import find_movies_job
+
+scheduler = django_rq.get_scheduler('default')
+log = logging.getLogger(__name__)
+
+def clear_scheduled_jobs():
+    # Delete any existing jobs in the scheduler when the app starts up
+    for job in scheduler.get_jobs():
+        log.debug("Deleting scheduled job %s", job)
+        job.delete()
+
+
+def register_scheduled_jobs():
+    # do your scheduling here
+    sample_args = "put some stuff here?"
+
+    scheduler.cron(
+        "0 1 * * *",                            # Time for first execution, in UTC timezone (6:30AM)
+        func=find_movies_job,                   # Function to be queued
+        repeat=None                             # Repeat this number of times (None means repeat forever)
+    )
+
+class Command(rqscheduler.Command):
+    help = 'Schedule looking up movies'
+
+    def handle(self, *args, **kwargs):
+        # This is necessary to prevent dupes
+        clear_scheduled_jobs()
+        register_scheduled_jobs()
+        super(Command, self).handle(*args, **kwargs)
